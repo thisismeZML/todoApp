94709e2fa33f99f508e91b1af8d1c37c5e24a6b4
thisismeZML <thatismetheoneudunknow@gmail.com>
2024-02-17 14:44:18 +0630

    tailwind setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  fontFamily: {
+    sans: ["Roboto", "Montserrat"],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
